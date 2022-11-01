76c2887536f6eef98d9e689cf02786001ac097ac
beyzanurgunaydin <117175696+beyzanurgunaydin@users.noreply.github.com>
2022-11-01 15:39:41 +0300

    Add files via upload

---
+++ logInPage/logInPage/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logInPage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "admin" && textBox2.Text == "123")
+            {
+                MessageBox.Show("Login Successfull!");
+            }
+            else
+            {
+                MessageBox.Show("Login failed. User not found.");
+            }
+        }
+    }
+}
